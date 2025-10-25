bbc31104839bf9ff34f48b355fc3f713fe3b48f1
Sakthivel.B <sakthivel12122005@gmail.com>
2025-10-25 16:28:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function showNavbar() {
+    sidenav.style.left = 0
+    sidenav.style.display = "block"
+
+}
+function closeNavbar() {
+    sidenav.style.left = -60
+    sidenav.style.display = "none"
+}
